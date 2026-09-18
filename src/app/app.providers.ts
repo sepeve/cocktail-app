9104f13c50751f9fed6eec3482a402b403464bc7
Sergio <spedersen86@gmail.com>
2026-09-18 12:48:48 +0200

    Change icon initializer to provider file

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,8 +1,23 @@
 import { inject } from '@angular/core';
 import { AppConfigService } from '../core/services/app-config.service'
 import { environment } from '../environments/environment.development';
+import { matChevronBackwardSharp } from '@ng-icons/material-symbols/sharp';
+import { lucideAward, lucideMartini, lucideMoon, lucideSun } from '@ng-icons/lucide';
+import { matHomeOutline, matFiberNewOutline, matAutorenewFillOutline, matDoNotDisturbOnOutline } from '@ng-icons/material-symbols/outline';
 
 export const initializeApp = () => {
     const appConfigService = inject(AppConfigService);
     return appConfigService.loadConfig(environment.configPath);
 }
+
+export const appIcons = (): Record<string, string> => ({
+    'chevron-left': matChevronBackwardSharp,
+    'glass': lucideMartini,
+    'award': lucideAward,
+    'sun': lucideSun,
+    'moon': lucideMoon,
+    'home': matHomeOutline,
+    'latest': matFiberNewOutline,
+    'random': matAutorenewFillOutline,
+    'not': matDoNotDisturbOnOutline,
+})
