3c92779357b003702e75a27eb0788753163c1c77
Sergio <spedersen86@gmail.com>
2026-09-18 12:45:01 +0200

    Change icon initializer to provider file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,10 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { initializeApp } from './app.providers';
+import { appIcons, initializeApp } from './app.providers';
 import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { loaderInterceptor } from '../core/interceptors/loader.interceptor';
 import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
-import { matChevronBackwardSharp } from '@ng-icons/material-symbols/sharp';
-import { lucideAward, lucideMartini, lucideMoon, lucideSun } from '@ng-icons/lucide';
-import { matHomeOutline, matFiberNewOutline, matAutorenewFillOutline, matDoNotDisturbOnOutline } from '@ng-icons/material-symbols/outline';
 import { provideZard } from '@/shared/core/provider/providezard';
 
 export const appConfig: ApplicationConfig = {
@@ -19,8 +16,8 @@ export const appConfig: ApplicationConfig = {
         provideBrowserGlobalErrorListeners(),
         provideRouter(routes, withComponentInputBinding()),
         provideHttpClient(withInterceptors([loaderInterceptor]), withInterceptorsFromDi()),
-        provideNgIconsConfig({ size: '1em' }),
+        provideNgIconsConfig({ size: '1.2em' }),
+        provideIcons(appIcons()),
         provideZard(),
-        provideIcons({ matChevronBackwardSharp, lucideMartini, lucideAward, lucideSun, lucideMoon, matHomeOutline, matFiberNewOutline, matAutorenewFillOutline, matDoNotDisturbOnOutline }),
     ]
 };
